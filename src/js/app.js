b159c01df7725a870399a54edf0c1621c05661d5
Léa Buguinet <lea@agencetaurine.fr>
2023-01-23 18:25:13 +0100

    animation icon WIP

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,35 @@
 
+let paths = document.querySelectorAll('path');
+
+//fillSvgPaths()
+
+//document.addEventListener('scroll', fillSvgPaths)
+
+function fillSvgPaths() {
+
+    let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+  console.log(scrollPercentage)
+    
+    for (var i = 0; i < paths.length; i++) {
+        let path = paths[i];
+
+        let pathLength = path.getTotalLength();
+
+        path.style.strokeDasharray = pathLength;
+        path.style.strokeDashoffset = pathLength;
+
+        let drawLength = pathLength * (scrollPercentage);
+
+        path.style.strokeDashoffset = pathLength - drawLength;
+    }
+}
+
+
+Array.from(document.getElementsByClassName("cls-1")).forEach(pathElement => {
+  pathElement.setAttribute('style', 'stroke-dasharray:'+pathElement.getTotalLength()+';stroke-dashoffset:'+pathElement.getTotalLength())
+  console.log('first')
+})
+
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
@@ -20,3 +51,11 @@ ScrollTrigger.create({
   scrub: true,
   pin: true
 });
+
+
+
+/* gsap.to(".icon-painting", {
+  scrollTrigger: ".transition-item", // start the animation when ".box" enters the viewport (once)
+  x: 500,
+  duration: 3,
+}); */
